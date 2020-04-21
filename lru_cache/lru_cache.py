da45b14ebf8b51a42977eb44b9a069135fdd6047
taterntots <matthew.a.weidner@gmail.com>
2020-04-21 18:29:00 -0400

    solved the lru_cache problem

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -13,7 +13,7 @@ class LRUCache:
     def __init__(self, limit=10):
         self.limit = limit # Limit Keeps track of the maximum number of nodes available to hold
         self.size = 0 # Size keeps track of the number of nodes
-        self.storage = DoublyLinkedList() # DLL to store order
+        self.list = DoublyLinkedList() # DLL that holds key:value pairs in the correct order
         self.hash_storage = {} # Dictionary to store key:value pairs
 
     """
@@ -24,13 +24,16 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        # If the key is not in our hash table, do nothing (nothing to retrieve)
+        # If the key is not in our hash table
         if key not in self.hash_storage:
+            # Do nothing (nothing to retrieve)
             return None
-        # If the key is in our hash table, return that keys value and moves it to the front (head)
+        # If the key is in our hash table
         else:
-            self.storage.move_to_front(self.hash_storage[key])
-            return self.hash_storage[key].value
+            # Move it to the front (head)
+            self.list.move_to_front(self.hash_storage[key])
+            # Return that keys value
+            return self.hash_storage[key].value[1]
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -46,22 +49,21 @@ class LRUCache:
         # If the key-value pair is already in our cache
         if key in self.hash_storage:
             # Replace the old key's value with the new one
-            self.hash_storage[key].value = value
-        # Else if the cache is at maximum capacity
-        elif self.limit > 10:
-            # Remove the oldest entry to make room (tail)
-            self.storage.remove_from_tail()
-            # Add the new entry to the front (head)
-            self.storage.add_to_head(key)
-        # Else add the key-value pair to the cache to the front (head)
-        else:
-            self.storage.add_to_head(key)
-
-list = {
-    'derp': 4, 
-    'flerp': 3
-    }
-
-LRUCache(3)
-
-print(DoublyLinkedList(list))
+            self.hash_storage[key].value = (key, value)
+            # Move the updated key:value pairing to the front
+            self.list.move_to_front(self.hash_storage[key])
+            return
+        
+        # If the cache is at maximum capacity
+        if self.size == self.limit:
+            # Remove the oldest entry from the cache dictionary and DLL to make room (tail)
+            del self.hash_storage[self.list.tail.value[0]] # Removes LRU from the cache dictionary
+            self.list.remove_from_tail() # Removes LRU from the DLL
+            # Decrease the size of the cache by one
+            self.size += 1
+        
+        # Otherwise add the new key-value pair to the cache and move to front (head)
+        self.list.add_to_head((key, value)) # adds to the DLL
+        self.hash_storage[key] = self.list.head # adds to the cache dictionary
+        # Increase the size of the cache by one
+        self.size += 1
