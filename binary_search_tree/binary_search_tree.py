04e1da931b0eca799e13fe5d08b3c83954bf9f82
taterntots <matthew.a.weidner@gmail.com>
2020-04-22 16:25:13 -0400

    think I solved the contains method for binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -21,24 +21,53 @@ class BinarySearchTree:
             print(f'{value} is less than {self.value}')
             if self.left:
                 self.left.insert(value)
-            # If the left side is empty
+            # Else if the left side is empty
             else:
                 # Assign the empty node to be our value
                 self.left = BinarySearchTree(value)
-        # Else if the given value is greater than or equal to the root value
+        # Otherwise, if the given value is greater than or equal to the root value
+       
         else: 
             # Check if self.right is a valid node
+            print(f'{value} is greater than or equal to {self.value}')
             if self.right:
                 self.right.insert(value)
-            # If the right side is empty
+            # Else if the right side is empty
             else:
                 # Assign the empty node to be our value
                 self.right = BinarySearchTree(value)
+                print(self.right)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # If the target value is equal to the root value, return true
+        if target == self.value:
+            print(f'True: {target} is equal to {self.value}')
+            return True
+
+        # If the target value is less than the root value
+        if target < self.value:
+            # And if there are no more left branches to go through, return false
+            if self.left == None:
+                print(f'False: {target} not found in left half')
+                return False
+            # Otherwise, if a node is found
+            else:
+                # Recursively keep going left
+                return self.left.contains(target)
+
+        # If the target value is greater than the root value
+        else:
+            # And if there are no more right branches to go through, return false
+            print(f'{target} is greater than {self.value}')
+            if self.right == None:
+                print(f'False: {target} not found in right half')
+                return False
+            # Otherwise, if a node is found
+            else:
+                # Recursively keep going right
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -77,8 +106,10 @@ class BinarySearchTree:
     def post_order_dft(self, node):
         pass
 
-bs = BinarySearchTree(9)
+bs = BinarySearchTree(10)
 
 print(bs)
 
 bs.insert(8)
+bs.insert(3)
+bs.contains(5)
